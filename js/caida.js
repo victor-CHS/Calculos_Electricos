fc63076b8dcf953f6dd877d345235da7327c40b2
victor-CHS <victor.chan92@hotmail.com>
2025-07-17 16:04:37 -0600

    segunda entrega

--- js/caida.js
+++ js/caida.js
@@ -1,26 +1,89 @@
+
 var seccionTrans = [1.31, 2.08, 3.31, 5.26, 8.37, 13.30, 21.20, 33.60, 53.49, 67.43, 85.01, 107.20];
-var seccion = [];
+var seccion = "";
 
 for (var i = 0; i < seccionTrans.length; i++) {
     seccion += "<option>" + seccionTrans[i] + "</option>"
 }
 document.getElementById("cable").innerHTML = "<option>" + seccion + "</option>"
 
+
 let calcular = document.getElementById("calcular")
 let distancia = document.getElementById("distancia")
 let corriente = document.getElementById("corriente")
-//let seccion = document.getElementById("seccion")
+let cable = document.getElementById("cable")
 const cuatro = 4
-const voltaje = 127
+const volt = 127
 
 calcular.onclick = () => {
     let resultado1 = cuatro * distancia.value * corriente.value
-    let resultado2 = voltaje.value * seccion.value
+    let resultado2 = volt * cable.value
     let resultadoFinal = resultado1 / resultado2
 
     let resultadoCalculo = document.getElementById("resultadoCalculo")
-    resultadoCalculo.innerText = "El resultado de la caida de tension es: " + resultadoFinal + " %"
-    print.innerText = resultadoCalculo 
+    if (resultadoFinal <= 3) {
+        resultadoCalculo.innerText = "El resultado de la caida de tension es: " + resultadoFinal + " %"
+        print.innerText = resultadoCalculo
+    } else if (resultadoFinal > 3) {
+        resultadoCalculo.innerText = "El resultado de la caida de tension es: " + resultadoFinal + " %,\n " + "se debera elegir otro calibre"
+        print.innerText = resultadoCalculo
+    }
+
+}
+
+
+document.getElementById("cableUno").innerHTML = "<option>" + seccion + "</option>"
+
+let calcularUno = document.getElementById("calcularUno")
+let distanciaUno = document.getElementById("distanciaUno")
+let corrienteUno = document.getElementById("corrienteUno")
+let voltajeUno = document.getElementById("voltajeUno")
+let cableUno = document.getElementById("cableUno")
+const numeroDos = 2
+
+calcularUno.onclick = () => {
+    let resultadoUno = numeroDos * distanciaUno.value * corrienteUno.value
+    let resultadoDos = voltajeUno.value * cableUno.value
+    let resultadoFinal = resultadoUno / resultadoDos
+
+    let resultadoCalculoUno = document.getElementById("resultadoCalculoUno")
+    if (resultadoFinal <= 3) {
+        resultadoCalculoUno.innerText = "El resultado de la caida de tension es: " + resultadoFinal + " %"
+        print.innerText = resultadoCalculoUno
+    } else if (resultadoFinal > 3) {
+        resultadoCalculoUno.innerText = "El resultado de la caida de tension es: " + resultadoFinal + " %,\n " + "se debera elegir otro calibre"
+        print.innerText = resultadoCalculoUno
+    }
+}
+
+
+document.getElementById("cableDos").innerHTML = "<option>" + seccion + "</option>"
+
+function multiplicacion(a, b) {
+    return a * b
+}
+const multiConst = multiplicacion(2, 1.73)
+let calcularDos = document.getElementById("calcularDos")
+let distanciaDos = document.getElementById("distanciaDos")
+let corrienteDos = document.getElementById("corrienteDos")
+let voltajeDos = document.getElementById("voltajeDos")
+let cableDos = document.getElementById("cableDos")
+
+calcularDos.onclick = () => {
+    let resultado1 = multiConst * distanciaDos.value * corrienteDos.value
+    let resultado2 = voltajeDos.value * cableDos.value
+    let resultadoFinal = resultado1 / resultado2
+
+    let resultadoCalculoDos = document.getElementById("resultadoCalculoDos")
+    if (resultadoFinal <= 3) {
+        resultadoCalculoDos.innerText = "El resultado de la caida de tension es: " + resultadoFinal + " %"
+        print.innerText = resultadoCalculoDos
+    } else if (resultadoFinal > 3) {
+        resultadoCalculoDos.innerText = "El resultado de la caida de tension es: " + resultadoFinal + " %,\n " + "se debera elegir otro calibre"
+        print.innerText = resultadoCalculoDos
+    }
+
+
 
 }
 
@@ -31,21 +94,5 @@ calcular.onclick = () => {
 
 
 
-// function caidaDeTension1() {
-//     const cuatro = 4
-//     const voltaje = 127
-//     let distancia = parseInt(prompt("Ingrese la distancia en ml"))
-//     let corriente = parseInt(prompt("Ingrese la corriente nominal en A"))
-//     for (let i = 0; i < calibres.length; i++) {
-//         console.log(calibres[i])
-//     }
-//     let seccion = parseInt(prompt("Ingrese la seccion transversal del calibre a usar"))
-//     let resultado1 = cuatro * distancia * corriente
-//     let resultado2 = voltaje * seccion
-//     let resultadoFinal = resultado1 / resultado2
-//     if (resultadoFinal <= 3) {
-//         alert("el resultado de la caida de tension es " + resultadoFinal + " %")
-//     } else if (resultadoFinal > 3) {
-//         alert("el resultado de la caida de tension es " + resultadoFinal + " % " + " se debera seleccionar otro calibre ")
-//     }
-// }
+
+
--- js/main.js
+++ js/main.js
@@ -6,19 +6,25 @@ let voltaje = document.getElementById("voltaje")
 const factorDePotencia = 0.9
 
 calcular.onclick = () => {
-    let resultado = voltaje.value*factorDePotencia
-    let resultadoFinal = potencia.value/resultado
+    let resultado = voltaje.value * factorDePotencia
+    let resultadoFinal = potencia.value / resultado
 
     let resultadoCalculo = document.getElementById("resultadoCalculo")
     resultadoCalculo.innerText = "El resultado de la corriente es: " + resultadoFinal + " A"
-    print.innerText = resultadoCalculo    
+    print.innerText = resultadoCalculo
+
+    localStorage.setItem("resultado", JSON.stringify(resultadoFinal));
+
 }
 
- 
+reiniciar.onclick = () => {
+    localStorage.removeItem("resultado");
+
+}
 
 function multiplicacion(a, b) {
     return a * b
-}    
+}
 const multiConst = multiplicacion(1.73, 0.9)
 let calcularUno = document.getElementById("calcularUno")
 let potenciaUno = document.getElementById("potenciaUno")
@@ -26,99 +32,11 @@ let voltajeUno = document.getElementById("voltajeUno")
 
 
 calcularUno.onclick = () => {
-    let resultado = multiConst*voltajeUno.value
-    let resultadoFinal = potenciaUno.value/resultado
+    let resultado = multiConst * voltajeUno.value
+    let resultadoFinal = potenciaUno.value / resultado
 
     let resultadoCalculoUno = document.getElementById("resultadoCalculoUno")
     resultadoCalculoUno.innerText = "El resultado de la corriente es: " + resultadoFinal + " A"
-    print.innerText = resultadoCalculoUno    
+    print.innerText = resultadoCalculoUno
 }
-
-
-
-
-
-// function caidaDeTension1() {
-//     const cuatro = 4
-//     const voltaje = 127
-//     let distancia = parseInt(prompt("Ingrese la distancia en ml"))
-//     let corriente = parseInt(prompt("Ingrese la corriente nominal en A"))
-//     for (let i = 0; i < calibres.length; i++) {
-//         console.log(calibres[i])
-//     }
-//     let seccion = parseInt(prompt("Ingrese la seccion transversal del calibre a usar"))
-//     let resultado1 = cuatro * distancia * corriente
-//     let resultado2 = voltaje * seccion
-//     let resultadoFinal = resultado1 / resultado2
-//     if (resultadoFinal <= 3) {
-//         alert("el resultado de la caida de tension es " + resultadoFinal + " %")
-//     } else if (resultadoFinal > 3) {
-//         alert("el resultado de la caida de tension es " + resultadoFinal + " % " + " se debera seleccionar otro calibre ")
-//     }
-// }
-
-// function caidaDeTension2() {
-//     const numeroDos = 2
-//     let distancia = parseInt(prompt("Ingrese la distancia en ml"))
-//     let corriente = parseInt(prompt("Ingrese la corriente nominal en A"))
-//     let voltaje = parseInt(prompt("Ingrese el voltaje, 127 o 220 V"))
-//     for (let i = 0; i < calibres.length; i++) {
-//         console.log(calibres[i])
-//     }
-//     let seccion = parseInt(prompt("Ingrese la seccion transversal del calibre a usar"))
-//     let resultado1 = numeroDos * distancia * corriente
-//     let resultado2 = voltaje * seccion
-//     let resultadoFinal = resultado1 / resultado2
-//     if (resultadoFinal <= 3) {
-//         alert("el resultado de la caida de tension es " + resultadoFinal + " %")
-//     } else if (resultadoFinal > 3) {
-//         alert("el resultado de la caida de tension es " + resultadoFinal + " % " + " se debera seleccionar otro calibre ")
-//     }
-// }
-
-// function caidaDeTension3() {
-//     function multiplicacion(a, b) {
-//         return a * b
-//     }
-//     const multiConst = multiplicacion(2, 1.73)
-//     let distancia = parseInt(prompt("Ingrese la distancia en ml"))
-//     let corriente = parseInt(prompt("Ingrese la corriente nominal en A"))
-//     let voltaje = parseInt(prompt("Ingrese el voltaje entre fases"))
-//     for (let i = 0; i < calibres.length; i++) {
-//         console.log(calibres[i])
-//     }
-//     let seccion = parseInt(prompt("Ingrese la seccion transversal del calibre a usar"))
-//     let resultado1 = multiConst * distancia * corriente
-//     let resultado2 = voltaje * seccion
-//     let resultadoFinal = resultado1 / resultado2
-//     if (resultadoFinal <= 3) {
-//         alert("el resultado de la caida de tension es " + resultadoFinal + " %")
-//     } else if (resultadoFinal > 3) {
-//         alert("el resultado de la caida de tension es " + resultadoFinal + " % " + " se debera seleccionar otro calibre ")
-//     }
-// }
-
-// let menu = parseInt(prompt("1-Corriente Electrica Monofasica \n 2-Corriete Electrica Trifasica \n 3-Caida de Tension Monofasica \n 4-Caida de Tension Bifasica \n 5-Caida de Tension Trifasica \n 6-salir"))
-// while (menu !== 6) {
-//     switch (menu) {
-//         case 1:
-//             monofasica()
-//             break
-//         case 2:
-//             trifasica()
-//             break
-//         case 3:
-//             caidaDeTension1()
-//             break
-//         case 4:
-//             caidaDeTension2()
-//             break
-//         case 5:
-//             caidaDeTension3()
-//             break
-//         default:
-//             alert("Opcion incorrecta")
-//     }
-//     menu = parseInt(prompt("1-Corriente Electrica Monofasica \n 2-Corriete Electrica Trifasica \n 3-Caida de Tension Monofasica \n 4-Caida de Tension Bifasica \n 5-Caida de Tension Trifasica \n 6-salir"))
-// }
 
