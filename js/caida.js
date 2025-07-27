5fa0f85e9ed9662135a9015ef7a6a3c5fb9b41db
victor-CHS <victor.chan92@hotmail.com>
2025-07-27 13:14:33 -0600

    se agreaga sweetalAlert2 y funciones de orden superior

--- js/caida.js
+++ js/caida.js
@@ -1,12 +1,13 @@
 
-var seccionTrans = [1.31, 2.08, 3.31, 5.26, 8.37, 13.30, 21.20, 33.60, 53.49, 67.43, 85.01, 107.20];
-var seccion = "";
+let seccionTrans = [1.31, 2.08, 3.31, 5.26, 8.37, 13.30, 21.20, 33.60, 53.49, 67.43, 85.01, 107.20];
+let seccion = "";
 
-for (var i = 0; i < seccionTrans.length; i++) {
+for (let i = 0; i < seccionTrans.length; i++) {
     seccion += "<option>" + seccionTrans[i] + "</option>"
 }
-document.getElementById("cable").innerHTML = "<option>" + seccion + "</option>"
 
+//Caida de tension monofasico
+document.getElementById("cable").innerHTML = "<option>" + seccion + "</option>"
 
 let calcular = document.getElementById("calcular")
 let distancia = document.getElementById("distancia")
@@ -20,18 +21,51 @@ calcular.onclick = () => {
     let resultado2 = volt * cable.value
     let resultadoFinal = resultado1 / resultado2
 
+    localStorage.setItem("resultado", JSON.stringify(resultadoFinal));
+    let dato = JSON.parse(localStorage.getItem("resultado"))
+
     let resultadoCalculo = document.getElementById("resultadoCalculo")
-    if (resultadoFinal <= 3) {
-        resultadoCalculo.innerText = "El resultado de la caida de tension es: " + resultadoFinal + " %"
+    if (dato <= 3) {
+        resultadoCalculo.innerText = "El resultado de la caida de tension es: \n" + dato + " %"
         print.innerText = resultadoCalculo
-    } else if (resultadoFinal > 3) {
-        resultadoCalculo.innerText = "El resultado de la caida de tension es: " + resultadoFinal + " %,\n " + "se debera elegir otro calibre"
+    } else if (dato > 3) {
+        resultadoCalculo.innerText = "El resultado de la caida de tension es: " + dato + " %,\n " + "se debera elegir otro calibre"
         print.innerText = resultadoCalculo
     }
-
 }
 
+limpiar.onclick = () => {
+    localStorage.removeItem("resultado");
 
+    let resultadoCalculo = document.getElementById("resultadoCalculo")
+    resultadoCalculo.innerText = " "
+    print.innerText = resultadoCalculo
+}
+
+const sectionFormulario = document.getElementById("sectionFormulario");
+const reiniciar = document.getElementById("reiniciar");
+
+function limpiarCampos(form, accion) {
+    const elementos = form.elements;
+    for (let i = 0; i < elementos.length; i++) {
+        const campo = elementos[i];
+        if (campo.tagName !== 'BUTTON' && campo.type !== 'submit') {
+            accion(campo);
+        }
+    }
+}
+
+function limpiarValor(campo) {
+    campo.value = '';
+}
+
+reiniciar.addEventListener("click", () => {
+    limpiarCampos(sectionFormulario, limpiarValor);
+
+});
+
+
+//Caida de tension Bifasica
 document.getElementById("cableUno").innerHTML = "<option>" + seccion + "</option>"
 
 let calcularUno = document.getElementById("calcularUno")
@@ -46,17 +80,51 @@ calcularUno.onclick = () => {
     let resultadoDos = voltajeUno.value * cableUno.value
     let resultadoFinal = resultadoUno / resultadoDos
 
+    localStorage.setItem("resultado", JSON.stringify(resultadoFinal));
+    let dato = JSON.parse(localStorage.getItem("resultado"))
+
     let resultadoCalculoUno = document.getElementById("resultadoCalculoUno")
-    if (resultadoFinal <= 3) {
-        resultadoCalculoUno.innerText = "El resultado de la caida de tension es: " + resultadoFinal + " %"
+    if (dato <= 3) {
+        resultadoCalculoUno.innerText = "El resultado de la caida de tension es: \n" + dato + " %"
         print.innerText = resultadoCalculoUno
-    } else if (resultadoFinal > 3) {
-        resultadoCalculoUno.innerText = "El resultado de la caida de tension es: " + resultadoFinal + " %,\n " + "se debera elegir otro calibre"
+    } else if (dato > 3) {
+        resultadoCalculoUno.innerText = "El resultado de la caida de tension es: " + dato + " %,\n " + "se debera elegir otro calibre"
         print.innerText = resultadoCalculoUno
     }
 }
 
+limpiarUno.onclick = () => {
+    localStorage.removeItem("resultado");
 
+    let resultadoCalculoUno = document.getElementById("resultadoCalculoUno")
+    resultadoCalculoUno.innerText = " "
+    print.innerText = resultadoCalculoUno
+}
+
+const sectionFormularioUno = document.getElementById("sectionFormularioUno");
+const reiniciarUno = document.getElementById("reiniciarUno");
+
+function limpiarCampos(form, accion) {
+    const elementos = form.elements;
+    for (let i = 0; i < elementos.length; i++) {
+        const campo = elementos[i];
+        if (campo.tagName !== 'BUTTON' && campo.type !== 'submit') {
+            accion(campo);
+        }
+    }
+}
+
+function limpiarValor(campo) {
+    campo.value = '';
+}
+
+reiniciarUno.addEventListener("click", () => {
+    limpiarCampos(sectionFormularioUno, limpiarValor);
+
+});
+
+
+//Caida de tension Trifasica
 document.getElementById("cableDos").innerHTML = "<option>" + seccion + "</option>"
 
 function multiplicacion(a, b) {
@@ -74,22 +142,61 @@ calcularDos.onclick = () => {
     let resultado2 = voltajeDos.value * cableDos.value
     let resultadoFinal = resultado1 / resultado2
 
+    localStorage.setItem("resultado", JSON.stringify(resultadoFinal));
+    let dato = JSON.parse(localStorage.getItem("resultado"))
+
     let resultadoCalculoDos = document.getElementById("resultadoCalculoDos")
-    if (resultadoFinal <= 3) {
-        resultadoCalculoDos.innerText = "El resultado de la caida de tension es: " + resultadoFinal + " %"
+    if (dato <= 3) {
+        resultadoCalculoDos.innerText = "El resultado de la caida de tension es: " + dato + " %"
         print.innerText = resultadoCalculoDos
-    } else if (resultadoFinal > 3) {
-        resultadoCalculoDos.innerText = "El resultado de la caida de tension es: " + resultadoFinal + " %,\n " + "se debera elegir otro calibre"
+    } else if (dato > 3) {
+        resultadoCalculoDos.innerText = "El resultado de la caida de tension es: " + dato + " %,\n " + "se debera elegir otro calibre"
         print.innerText = resultadoCalculoDos
     }
-
-
-
 }
 
+limpiarDos.onclick = () => {
+    localStorage.removeItem("resultado");
 
+    let resultadoCalculoDos = document.getElementById("resultadoCalculoDos")
+    resultadoCalculoDos.innerText = " "
+    print.innerText = resultadoCalculoDos
+}
 
+const sectionFormularioDos = document.getElementById("sectionFormularioDos");
+const reiniciarDos = document.getElementById("reiniciarDos");
 
+function limpiarCampos(form, accion) {
+    const elementos = form.elements;
+    for (let i = 0; i < elementos.length; i++) {
+        const campo = elementos[i];
+        if (campo.tagName !== 'BUTTON' && campo.type !== 'submit') {
+            accion(campo);
+        }
+    }
+}
+
+function limpiarValor(campo) {
+    campo.value = '';
+}
+
+reiniciarDos.addEventListener("click", () => {
+    limpiarCampos(sectionFormularioDos, limpiarValor);
+
+});
+
+function reiniciarAlert() {
+    Swal.fire({
+        position: "center-center",
+        icon: "success",
+        title: "Formulario Reiniciado",
+        showConfirmButton: false,
+        timer: 1500,
+        background: "#000",
+        color: "#fff",
+        iconColor: "#00FF00"
+    });
+}
 
 
 
--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 
-
+//Calculo de cooriente electrica Monofasica 
 let calcular = document.getElementById("calcular")
 let potencia = document.getElementById("potencia")
 let voltaje = document.getElementById("voltaje")
@@ -9,19 +9,47 @@ calcular.onclick = () => {
     let resultado = voltaje.value * factorDePotencia
     let resultadoFinal = potencia.value / resultado
 
+    localStorage.setItem("resultado", JSON.stringify(resultadoFinal));
+    let dato = JSON.parse(localStorage.getItem("resultado"))
+
     let resultadoCalculo = document.getElementById("resultadoCalculo")
-    resultadoCalculo.innerText = "El resultado de la corriente es: " + resultadoFinal + " A"
+    resultadoCalculo.innerText = "El resultado de la corriente es: \n" + dato + " A"
     print.innerText = resultadoCalculo
 
-    localStorage.setItem("resultado", JSON.stringify(resultadoFinal));
-
 }
 
-reiniciar.onclick = () => {
+limpiar.onclick = () => {
     localStorage.removeItem("resultado");
 
+    let resultadoCalculo = document.getElementById("resultadoCalculo")
+    resultadoCalculo.innerText = " "
+    print.innerText = resultadoCalculo
 }
 
+const sectionFormulario = document.getElementById("sectionFormulario");
+const reiniciarForm = document.getElementById("reiniciarForm");
+
+function limpiarCampos(form, accion) {
+    const elementos = form.elements;
+    for (let i = 0; i < elementos.length; i++) {
+        const campo = elementos[i];
+        if (campo.tagName !== 'BUTTON' && campo.type !== 'submit') {
+            accion(campo);
+        }
+    }
+}
+
+function limpiarValor(campo) {
+    campo.value = '';
+}
+
+reiniciarForm.addEventListener("click", () => {
+    limpiarCampos(sectionFormulario, limpiarValor);
+
+});
+
+
+//Calculo de corriente electrica trifasica
 function multiplicacion(a, b) {
     return a * b
 }
@@ -33,10 +61,55 @@ let voltajeUno = document.getElementById("voltajeUno")
 
 calcularUno.onclick = () => {
     let resultado = multiConst * voltajeUno.value
-    let resultadoFinal = potenciaUno.value / resultado
+    let resultadoFinal1 = potenciaUno.value / resultado
+
+    localStorage.setItem("resultado", JSON.stringify(resultadoFinal1));
+    let datoUno = JSON.parse(localStorage.getItem("resultado"))
 
     let resultadoCalculoUno = document.getElementById("resultadoCalculoUno")
-    resultadoCalculoUno.innerText = "El resultado de la corriente es: " + resultadoFinal + " A"
+    resultadoCalculoUno.innerText = "El resultado de la corriente es: \n" + datoUno + " A"
     print.innerText = resultadoCalculoUno
 }
 
+limpiarUno.onclick = () => {
+    localStorage.removeItem("resultado");
+
+    let resultadoCalculoUno = document.getElementById("resultadoCalculoUno")
+    resultadoCalculoUno.innerText = " "
+    print.innerText = resultadoCalculoUno
+}
+
+const sectionFormularioUno = document.getElementById("sectionFormularioUno");
+const reiniciarFormUno = document.getElementById("reiniciarFormUno");
+
+function limpiarCamposUno(form, accion) {
+    const elementos = form.elements;
+    for (let i = 0; i < elementos.length; i++) {
+        const campo = elementos[i];
+        if (campo.tagName !== 'BUTTON' && campo.type !== 'submit') {
+            accion(campo);
+        }
+    }
+}
+
+function limpiarValorUno(campo) {
+    campo.value = '';
+}
+
+reiniciarFormUno.addEventListener("click", () => {
+    limpiarCamposUno(sectionFormularioUno, limpiarValorUno);
+    //alert("Formulario limpiado");
+});
+
+function reiniciarAlert() {
+    Swal.fire({
+        position: "center-center",
+        icon: "success",
+        title: "Formulario Reiniciado",
+        showConfirmButton: false,
+        timer: 1500,
+        background: "#000",
+        color: "#fff",
+        iconColor: "#00FF00"
+    });
+}
